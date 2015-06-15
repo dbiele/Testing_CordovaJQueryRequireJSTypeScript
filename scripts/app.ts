a3df857a5a5875e265ccc60d503a3b764449b843
DeanB2015 <dean@invisionlearning.com>
2015-06-15 11:53:22 -0400

    Adding files to repository

---
+++ scripts/app.ts
@@ -0,0 +1,14 @@
+﻿import jq = require('jquery');
+module App {
+    "use strict";
+    export function initialize() {
+        console.log("initialize called");
+    }
+    document.addEventListener('deviceready', onDeviceReady, false);
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        console.log("cordova is ready: onDeviceReady");
+        console.log("Checking jQuery version number = " + jq.fn.jquery);
+    }
+}
+export = App;
---
+++ scripts/index.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path='typings/requirejs/require.d.ts' />
+import App = require('app');
+requirejs.config({
+    paths: {
+        jquery: 'lib/jquery-2.1.4'
+    }
+});
+require(['../cordova', 'app'], function (app) {
+    //The following line appears to be the problem. When uncommented it causes the error and looks for scripts/jquery.
+    //App.initialize();
+});
+// More information about RequireJS can be found @ http://requirejs.org/docs/api.html
